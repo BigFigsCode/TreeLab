209844c35849a955a823f0158a6825b99f5faa4e
BigFigsCode <58825803+BigFigsCode@users.noreply.github.com>
2025-01-11 14:17:34 -0800

    Update Comparable.java

---
+++ Comparable.java
@@ -0,0 +1,17 @@
+
+/**
+ * <h2>Comparable.java - An interface for the words in the getty and omit text to compare if one is greater than the other.</h2> 
+ * 
+ * @author Brandon Figueroa
+ * Palomar ID 010881556
+ */
+public interface Comparable
+{
+    /**
+     * compareTo is for comparing words for search tree
+     * @param o word
+     * @return the greater word
+     */
+    int compareTo(Object o);
+
+}
